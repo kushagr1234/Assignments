30de3f09db9e3b0883d5e5bfd35be2ba5404911e
kushagr1234 <111269171+kushagr1234@users.noreply.github.com>
2025-04-18 23:56:58 +0530

    Add files via upload

---
+++ Assignment 1 Task-1.py
@@ -0,0 +1,8 @@
+# perform basic operations add,sub,mul,div
+x=int(input("Enter a number:\t"))
+y=int(input("Enter another number:\t"))
+
+a=print("Your result for addition is:\t",x+y)
+s=print("Your result for subtraction is:\t",x-y)
+m=print("Your result for multiplication is:\t",x*y)
+d=print("Your result for division is:\t",x/y)
---
+++ Assignment 1 Task-2.py
@@ -0,0 +1,6 @@
+#Greeting someone
+
+x=input("Enter your prestigious first name:\t")
+y=input("Enter your prestigious second name:\t")
+
+print("Hello,",x+y,"Welcome to the python world")
