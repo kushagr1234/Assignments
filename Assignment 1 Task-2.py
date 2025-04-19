74441dba460a1b35c3ca9231ca9e00b8a8b72f6f
kushagr1234 <111269171+kushagr1234@users.noreply.github.com>
2025-04-19 16:22:10 +0530

    Update Assignment 1 Task-2.py

--- Assignment 1 Task-2.py
+++ Assignment 1 Task-2.py
@@ -3,4 +3,4 @@
 x=input("Enter your prestigious first name:\t")
 y=input("Enter your prestigious second name:\t")
 
-print("Hello,",x+y,"Welcome to the python world")
+print("Hello,",x+y,'!'"Welcome to the python world")
