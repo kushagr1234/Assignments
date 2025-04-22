5687b0db65670235570267c4a946cb21691d593d
kushagr1234 <111269171+kushagr1234@users.noreply.github.com>
2025-04-22 22:56:43 +0530

    Add files via upload

---
+++ Assignment 2 Task-2 (without for loop).py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number:\t"))
+d=int(input("Enter the comman difference:\t"))
+n=int(input("Enter the number of terms:\t"))
+
+s=n/2 * (2*a+(n-1)*d)
+
+
+print("The sum is:\t",s)
