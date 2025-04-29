3102e214345a17c6757cd680d0947401e18b06f3
kushagr1234 <111269171+kushagr1234@users.noreply.github.com>
2025-04-29 22:16:19 +0530

    Add files via upload

---
+++ Assignment3 Task-2.py
@@ -0,0 +1,25 @@
+num=int(input("Enter a number:\t"))
+
+#CALCULATING SQUARE ROOT=>
+
+import math
+
+
+sqr= math.sqrt(num)
+print("The square root of",num,"is:\t",sqr)
+
+
+
+#CALCULATING LOG=>
+
+
+log=math.log(num)
+print("The log of",num,"is:\t",log)
+
+
+#CALCULATING SINE(IN RADIANS)
+
+
+sine=math.sin(num)
+print("The sine of",num,"is:\t",sine)
+
